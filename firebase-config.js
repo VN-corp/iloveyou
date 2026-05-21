a1d3051b871a9df70448bcf026fd491ae58c3d85
dangthanhdat <dangthanhdat0209@gmail.com>
2026-05-21 22:27:17 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey:            "AIzaSyAp662RigHoY9oZEJ5RuRI-pr1i9C8F38c",
+    authDomain:        "trauastudiogroup.firebaseapp.com",
+    projectId:         "trauastudiogroup",
+    storageBucket:     "trauastudiogroup.appspot.com",
+    messagingSenderId: "1051790148888",
+    appId:             "1:1051790148888:web:1234567890abcdef"
+};
